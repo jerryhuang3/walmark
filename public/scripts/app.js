de2b9617abaa85acc6a9cfb5a0f9b1de4057ce01
jerryhuang3 <jerryhuang@utexas.edu>
2019-03-28 08:19:18 +0000

    added Foundation and Masonry styling and placeholder home page with header partials

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,20 @@ $(() => {
     url: "/api/users"
   }).done((users) => {
     for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
+      $("<div>").text(user.full_name).appendTo($("body"));
     }
   });;
+});
+
+$(document).ready(function(){
+  //Initialize Masonry Script
+  var $grid = $('.container').imagesLoaded( function() {
+    $grid.masonry({
+      itemSelector: '.links',
+      columnWidth: '.links',
+      horizontalOrder: true,
+      percentPosition: true,
+      stagger: 30
+    });
+  });
 });
