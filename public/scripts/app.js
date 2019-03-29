401296337b95d69cfeffc09195dbf486e609d301
karen1au <kagemikaka@hotmail.com>
2019-03-29 15:04:58 +0000

    added add comments feature

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,16 +4,16 @@ function escape(str) {
   return div.innerHTML;
 }
 
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.full_name).appendTo($("body"));
-    }
-  });
-});
+// $(() => {
+//   $.ajax({
+//     method: "GET",
+//     url: "/api/users"
+//   }).done((users) => {
+//     for(user of users) {
+//       $("<div>").text(user.full_name).appendTo($("body"));
+//     }
+//   });
+// });
 
 $(document).ready(function() {
   //Initialize Masonry Script
@@ -31,11 +31,24 @@ $(document).ready(function() {
   $(document).foundation();
 
 //get links page
-$('.container').on('click', '.links', function() {
-  $.ajax({
-    method: 'GET',
-    url: '/links/' + this.id
-  });
-})
+// function renderComments(comments) {
+//   for (let individual = 0; individual < comments.length; individual++) {
+//     $('.link-comments').prepend(createComment(comments[individual]));
+//   }
+// }
+// function loadTweets(){
+//   $.get(`http://localhost:8080/links/${this.id}/comments`, function (comments){
+//     renderTweets(comments);
+//   });
+// }
+// function createTweetElement(data) {
+//   let $comment = `
+//     <h4>Comments</h4>
+//     <img class="comment-avatar" src="<%=comment_avatar%>">
+//     <div class="comment-content">
+//         <span class="comment-fullname"><%=comment_name%></span>
+//         <span class="comment-date">5d</span>
+//         <p class="comment-text"><%=comment_text%></p>
+//     </div>`
 
 
--- routes/links.js
+++ routes/links.js
@@ -3,48 +3,53 @@
 const express = require('express');
 const linksRoutes  = express.Router();
 const LinkHelpers = require('../lib/link-helpers.js');
+const cookieSession = require('cookie-session');
 
 module.exports = (knex) => {
 
   linksRoutes.get("/", (req, res) => {
     Promise.all([
-      knex
-      .select('*')
-      .from('comments')
-      .join('links',{'links.id' : 'comments.link_id'})
+      knex.select('*').from('links')
+      .join('users',{'links.user_id' : 'users.id'})
       .where('links.id',1),
-      knex
-      .select('*')
-      .from('users')
+  knex.select('*').from('comments')
+      .join('users',{'comments.user_id' : 'users.id'})
+      .where('comments.link_id',1)
     ]).then((results) => {
         res.json(results)
     });
   });
 
-
-  linksRoutes.get("/:linkID", (req, res) => {
-    const linkID = req.params.linkID;
-    const response = knex.select('*').from('links')
-                         .join('users',{'links.user_id' : 'users.id'})
-                         .where('links.id',linkID)
-                         .then(function(results){
-                            let links = results[0];
-                              res.render('link', {
-                                session: req.session.userid,
-                                title: links.title,
-                                user: links.full_name,
-                                user_avatar: links.avatar,
-                                url: links.url,
-                                desc: links.description,
-                                create_date: links.create_date
-
-    });
+//getting comments
+  linksRoutes.get("/:linkId", (req, res) => {
+    const linkId = req.params.linkId;
+    const response = Promise.all([
+      knex.select('*').from('links')
+          .join('users',{'links.user_id' : 'users.id'})
+          .where('links.id',linkId),
+      knex.select('*').from('comments')
+          .join('users',{'comments.user_id' : 'users.id'})
+          .where('comments.link_id',linkId)])
+                .then(function(results){
+                  const links = results[0][0];
+                  const comments = results[1][0];
+                  const vartemplate = {
+                    session: req.session.userid,
+                    title: links.title,
+                    user: links.full_name,
+                    user_avatar: links.avatar,
+                    url: links.url,
+                    desc: links.description,
+                    create_date: links.create_date,
+                    comment_avatar: comments.avatar,
+                    comment_name: comments.full_name,
+                    comment_date: comments.create_date,
+                    comment_text: comments.text,
+                    link_id:linkId
+                  }
+                    res.render('link', vartemplate);
   });
-    // const comment_res = knex.select('*').from('comments')
-    //                         .join('links')
 });
-
-
 
   //displaying comments on one link
   linksRoutes.get("/:linkId/comments", (req, res) => {
@@ -58,34 +63,25 @@ module.exports = (knex) => {
   });
 
   //posting comment;
-  linksRoutes.post("/:link_id/comments", (req, res) => {
+  linksRoutes.post("/:linkId/comment", (req, res) => {
     if (!req.body.text) {
         res.status(400).json({ error: 'invalid request: no data in POST body'});
         return;
       }
-      const user = knex
-        .select('id').from('users').where('username','test');
-    //   const user = req.body.user ? req.body.user : UserHelpers.generateRandomUser();
-      const link = knex
-      .select('id').from('links').where('url','https://www.khanacademy.org/math');
-      const comment = {
-        user_id: user,
-        link_id: link,
-        content: {
-          text: req.body.text
-        },
-        created_at: Date.now(),
-        like_count: 0
-      };
-  
-      DataHelpers.saveTweet(tweet, (err) => {
-        if (err) {
-          res.status(500).json({ error: err.message });
-        } else {
-          res.status(201).send();
-        }
-      });
-  
+      const user_id = knex.select('id').from('users').where('username',req.session.userid),
+        link_id = req.params.linkId,
+        text = req.body.text,
+        // created_at = Date.now(),
+        like_count = 0
+      knex.insert([{user_id: user_id, link_id: link_id, text: text, create_date: knex.fn.now()}])
+          .into('comments').asCallback(function(err){
+            if (err) { 
+              res.status(500).json({ error: err.message });
+            } else {
+                console.log('YAY!');
+                res.status(201).send();
+            }
+        });
   })
 
   return linksRoutes;
