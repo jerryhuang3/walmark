e9cd4cd4d5253a3df3f907b8e0b2a4662ee3b14b
Harkamal Khabra <khabra07@gmail.com>
2019-03-29 21:51:43 +0000

    User board route

--- routes/profile.js
+++ routes/profile.js
@@ -5,21 +5,26 @@ const profileRoutes  = express.Router();
 
 module.exports = (knex) => {
 
-  profileRoutes.get("/:userID", (req, res) => {
-    const userID = req.params.userID;
-    const result = knex.select('*').from('users')
-                      .then(function(results){
-                        console.log(results);
-                         let users = results[0];
-      res.render('account_page', {
-        full_name: users.full_name,
-        user_avatar: users.avatar,
-        email: users.email,
-        id: req.session.userid,
-        username: users.username
+  // profile page
+profileRoutes.get("/:userID", (req, res) => {
+  const userID = req.params.userID;
+  const result = knex.select('*').from('users')
+                    .then(function(results){
+                      console.log(results);
+                       let users = results[0];
+    res.render('account_page', {
+      full_name: users.full_name,
+      user_avatar: users.avatar,
+      email: users.email,
+      id: req.session.userid,
+      username: users.username
     });
   });
 });
+
+profileRoutes.get("/:userID/boards/:boardID", (req, res) => {
+  res.render('user_boards')
+})
 
   return profileRoutes;
 }
