95864cf5b44e84a176704b1f04314697e89ec0ce
jerryhuang3 <jerryhuang@utexas.edu>
2019-03-30 00:00:36 +0000

    Home page randomizes database links

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,5 @@
+
+
 function escape(str) {
   var div = document.createElement('div');
   div.appendChild(document.createTextNode(str));
@@ -11,16 +13,7 @@ $(() => {
   }).done((users) => {
   });
 
-  //Initialize Masonry Script
-  var $grid = $('.container').imagesLoaded(function() {
-    $grid.masonry({
-      itemSelector: '.links',
-      columnWidth: '.links',
-      horizontalOrder: true,
-      percentPosition: true,
-      stagger: 30
-    });
-  });
+
 
   function createCommentElement(data) {
     let $comment = `
@@ -48,6 +41,62 @@ $(() => {
   };
 
   loadComments();
+
+
+
+
+
+
+
+// //Initialize Masonry Script
+//
+//
+// function loadHomeLinks() {
+//   $.get('/api/links', function(allLinks) {
+//     renderHomeLinks(allLinks);
+//   });
+// };
+// loadHomeLinks();
+//
+// function shuffle(a) {
+//   for (let i = a.length - 1; i > 0; i--) {
+//     const j = Math.floor(Math.random() * (i + 1));
+//     [a[i], a[j]] = [a[j], a[i]];
+//   }
+//   return a;
+// }
+//
+// function renderHomeLinks(allLinks) {
+//     let randomLinks = shuffle(allLinks);
+//
+//     for (let i = 0; i < randomLinks.length; i++) {
+//       $("#link-container").prepend(createHomeLinks(randomLinks[i])).masonry('prepended',createHomeLinks(randomLinks[i]));
+//     }
+//   let $grid = $('#link-container').imagesLoaded(function() {
+//     $grid.masonry({
+//       itemSelector: '.links',
+//       columnWidth: '.links',
+//       horizontalOrder: true,
+//       percentPosition: true,
+//     });
+//   });
+// };
+//
+// function createHomeLinks(randomLinks) {
+//   var myArray = ['200','300','400','500','600'];
+//   var randomItem = myArray[Math.floor(Math.random()*myArray.length)];
+//
+//   return $randomLinks = `
+//        <div class="links" xmlns="http://www.w3.org/1999/html">
+//           <a href="/links/${randomLinks.id}"><img src="https://picsum.photos/200/${randomItem}/?random" /></a>
+//           <h2>${randomLinks.title}</h2>
+//           <p>${randomLinks.description}</p>
+//        </div>`;
+// }
+
+
+
+
 
 });
 
