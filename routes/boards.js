6f73128fc64fe8e54953136c2ecf65e4030551e8
Harkamal Khabra <khabra07@gmail.com>
2019-03-31 01:40:42 +0000

    Very basic /boards/create form

--- routes/boards.js
+++ routes/boards.js
@@ -11,8 +11,22 @@ module.exports = (knex) => {
       .from("boards")
       .then((results) => {
       res.json(results);
-});
-});
+    });
+  });
+
+  boards.get("/create", (req, res) => {
+    const currentUser = req.session.userid;
+    if (!currentUser){
+      res.redirect('back');
+    } else {
+      knex
+      .select('*')
+      .from('users')
+      .then(function(results){
+        res.render("create_board");
+        })
+      }
+  });
 
   return boards;
 }
