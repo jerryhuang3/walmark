43dff9b3550012d9d8aec7d7724957410f2ee283
Harkamal Khabra <khabra07@gmail.com>
2019-03-31 03:38:04 +0000

    /boards route for page with all boards initialized

--- routes/boards.js
+++ routes/boards.js
@@ -6,14 +6,10 @@ const boards  = express.Router();
 module.exports = (knex) => {
 
   boards.get("/", (req, res) => {
-    knex
-    .select("*")
-      .from("boards")
-      .then((results) => {
-      res.json(results);
-    });
+    res.render("all_boards")
   });
 
+  // get from create form
   boards.get("/create", (req, res) => {
     const currentUser = req.session.userid;
     if (!currentUser){
@@ -27,6 +23,15 @@ module.exports = (knex) => {
         })
       }
   });
+
+  // post to database and redirect to /users/boards
+  boards.post("/create", (req, res) => {
+    if (!req.body) {
+      res.status(400).json({error: 'invalid request: no data in POST body'});
+      return;
+    }
+    res.redirect("/boards");
+  })
 
   return boards;
 }
