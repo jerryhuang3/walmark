ff8301b4a401d847a858be1b28bf2d8a70d708cb
Harkamal Khabra <khabra07@gmail.com>
2019-03-31 23:16:34 +0000

    Reorder routes in profile.js

--- routes/profile.js
+++ routes/profile.js
@@ -5,6 +5,41 @@ const profileRoutes  = express.Router();
 
 module.exports = (knex) => {
 
+// Create board
+profileRoutes.get("/:userID/board/create", (req, res) => {
+  const currentUser = req.session.userid;
+    if (!currentUser){
+      res.redirect('back');
+    } else {
+      Promise.all([
+      knex
+      .select('*')
+      .from('users')
+      .where('id', req.session.userid),
+      knex.select('title').from('boards').where('user_id',currentUser)
+      ])
+    .then(function(results){
+      const boards = results[1]
+      const templateVars = {
+      id: req.session.userid,
+      username: results.username,
+      full_name: results.full_name
+    }
+      res.render("create_board", templateVars);
+      })
+    }
+  });
+
+// Route for creating a board
+profileRoutes.post("/:userID/board/create", (req, res) => {
+  console.log(req.body)
+})
+
+// Route for updating boards
+profileRoutes.put("/:userID/board/:boardID/update", (req, res) => {
+
+})
+
 // User boards
 profileRoutes.get("/:userID/board/:boardID", (req, res) => {
   const userID = req.params.userID;
@@ -35,42 +70,6 @@ profileRoutes.get("/:userID/board/:boardID", (req, res) => {
       res.render('user_board', templateVars);
         })
     });
-
-// Create board
-profileRoutes.get("/:userID/board", (req, res) => {
-  const currentUser = req.session.userid;
-    if (!currentUser){
-      res.redirect('back');
-    } else {
-      Promise.all([
-      knex
-      .select('*')
-      .from('users')
-      .where('id', req.session.userid),
-      knex.select('title').from('boards').where('user_id',currentUser)
-      ])
-    .then(function(results){
-      const boards = results[1]
-      const templateVars = {
-      id: req.session.userid,
-      username: results.username,
-      full_name: results.full_name
-    }
-      res.render("create_board", templateVars);
-      })
-    }
-  });
-
-// Route for creating a board
-profileRoutes.post("/:userID/board", (req, res) => {
-  console.log(req.body)
-})
-
-// Route for updating boards
-profileRoutes.put("/:userID/board/:boardID/update", (req, res) => {
-
-})
-
 
   return profileRoutes;
 }
