bd2da89bdec4dca9a47e17e7260bf2d09698255c
jerryhuang3 <jerryhuang@utexas.edu>
2019-04-03 13:37:19 +0000

    add link-edit image

--- knexfile.js
+++ knexfile.js
@@ -23,7 +23,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL + '?ssl=true',
+    connection: postgres://npsbxrewnivohz:602c22aefc1e5f5ec52f993d7e4133e1c1345197b482f303adc92724a1710c25@ec2-174-129-10-235.compute-1.amazonaws.com:5432/deo4ml8l7acv24 + '?ssl=true',
     pool: {
       min: 2,
       max: 10
